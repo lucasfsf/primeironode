6807099def2a3384e3ff4d77f9609fb7469baece
Lucas Fonseca <lucasfreireri@gmail.com>
2020-09-04 12:22:16 -0300

    Apos Repositorio do TypeORM

--- src/database/migrations/1599132881410-CreateAppointments.ts
+++ src/database/migrations/1599132881410-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1599132881410
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
