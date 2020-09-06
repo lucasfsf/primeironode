86dd1960816025b6c5864cf18d6c8cf94bbc32dd
Lucas Fonseca <lucasfreireri@gmail.com>
2020-09-06 11:00:55 -0300

    Relacionamento nos models

--- src/database/migrations/1599132881410-CreateAppointments.ts
+++ src/database/migrations/1599132881410-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1599132881410
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1599241913406-CreateUsers.ts
+++ src/database/migrations/1599241913406-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1599241913406 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
