cb4b01370b2a9ad14e9b6aa3c4642e3463f1e376
Lucas Fonseca <lucasfreireri@gmail.com>
2020-09-25 16:00:23 -0300

    Started DDD -> separated files in modules

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,10 +2,10 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import AppError from '../errors/AppError';
+import AppError from '../../../shared/errors/AppError';
 
-import Appointment from '../models/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import Appointment from '../../../models/Appointment';
+import AppointmentsRepository from '../../../repositories/AppointmentsRepository';
 
 // only one method inside the service
 
